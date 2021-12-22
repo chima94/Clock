13374e095d7b707f144db3dce01be9bf40a25b55
chima94 <chimanwakis@gmail.com>
2021-12-22 12:04:54 +0100

    alarm screen

--- app/src/main/java/com/example/clock/ui/main/timer/TimerActions.kt
+++ app/src/main/java/com/example/clock/ui/main/timer/TimerActions.kt
@@ -2,4 +2,6 @@ package com.example.clock.ui.main.timer
 
 sealed class TimerActions{
     data class NumberClicked(val number: String): TimerActions()
+    object  ClearTimer: TimerActions()
+    object StartTimer: TimerActions()
 }
--- app/src/main/java/com/example/clock/ui/main/timer/TimerUI.kt
+++ app/src/main/java/com/example/clock/ui/main/timer/TimerUI.kt
@@ -1,24 +1,36 @@
 package com.example.clock.ui.main.timer
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Backspace
+import androidx.compose.material.icons.outlined.Pause
 import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import com.example.clock.R
 import com.example.clock.ui.component.AppBar
+import kotlinx.coroutines.delay
+import timber.log.Timber
+import java.lang.Math.PI
+import kotlin.math.sin
 
 
 @Composable
 fun TimerUI(){
+
     var hour by remember{ mutableStateOf("00")}
     var minutes by remember{ mutableStateOf("00")}
     var seconds by remember{ mutableStateOf("00")}
@@ -26,46 +38,83 @@ fun TimerUI(){
     val c by remember { mutableStateOf(time.toCharArray())}
     val c2 by remember{ mutableStateOf(ArrayList<Char>())}
     var counter by remember{ mutableStateOf(c.size - 1)}
+    var showTimerPlayBtn by remember{ mutableStateOf(false)}
     var startTimer by remember{ mutableStateOf(false)}
+    var isTimerRunning by remember{ mutableStateOf(false)}
+    var k by remember{ mutableStateOf(5)}
 
 
     TimerUIScaffold(
         hour = hour,
         minutes = minutes,
         seconds = seconds,
-        startTimer = startTimer
+        showTimerPlayBtn = showTimerPlayBtn,
+        startTimer = startTimer,
+        isTimerRunning = isTimerRunning
     ){action ->
         when(action){
             is TimerActions.NumberClicked ->{
                 if(c2.size < 6){
                     val currentNumber = action.number.single()
                     c2.add(currentNumber)
-                    if(counter < 5){
-                        var k = counter
-                        for(i in 0 until c2.size){
-                            c[k] = c2[i]
-                            k += 1
+                    if(c2[0] != '0'){
+                        if(counter < 5){
+                            var k = counter
+                            for(i in 0 until c2.size){
+                                c[k] = c2[i]
+                                k += 1
+                            }
+                        }else{
+                            c[counter] = currentNumber
+                            showTimerPlayBtn = true
                         }
+                        hour = String(c).substring(0, 2)
+                        minutes = String(c).substring(2, 4)
+                        seconds = String(c).substring(4, 6)
+                        counter -= 1
                     }else{
-                        c[counter] = currentNumber
-                        startTimer = true
+                        c2.clear()
                     }
+                }
+            }
+            TimerActions.ClearTimer ->{
+                if(c2.isNotEmpty()){
+                    k -= 1
+                    counter += 1
+                    c2.removeAt(c2.size - 1)
+                    c[counter] = '0'
                     hour = String(c).substring(0, 2)
                     minutes = String(c).substring(2, 4)
                     seconds = String(c).substring(4, 6)
-                    counter -= 1
+                    if(k < 0){
+                        showTimerPlayBtn = false
+                        k = 5
+                    }
+                }
+            }
+            TimerActions.StartTimer ->{
+                if(!startTimer && !isTimerRunning){
+                    startTimer = !startTimer
+                    isTimerRunning = !isTimerRunning
+                }else{
+                    isTimerRunning = !isTimerRunning
                 }
             }
         }
     }
 }
 
+
+
+
 @Composable
 private fun TimerUIScaffold(
     hour: String,
     minutes: String,
     seconds: String,
+    showTimerPlayBtn: Boolean,
     startTimer: Boolean,
+    isTimerRunning: Boolean,
     action : (TimerActions) -> Unit,
 ){
     Scaffold(
@@ -74,21 +123,36 @@ private fun TimerUIScaffold(
         },
         content = {
             Box(modifier = Modifier.fillMaxSize()){
-                TimerContent(
-                    modifier = Modifier
-                        .padding(top = 40.dp)
-                        .fillMaxWidth(),
-                    hour = hour,
-                    minutes = minutes,
-                    seconds = seconds,
-                    action = action,
-                    startTimer = startTimer
-                )
-                if(startTimer){
+                if(!startTimer){
+                    TimerContent(
+                        modifier = Modifier
+                            .padding(top = 40.dp)
+                            .fillMaxWidth(),
+                        hour = hour,
+                        minutes = minutes,
+                        seconds = seconds,
+                        action = action,
+                        showTimerPlayBtn = showTimerPlayBtn
+                    )
+                }else{
+                    Timer(
+                        totalTime = 100L * 1000L,
+                        handleColor = MaterialTheme.colors.primary,
+                        inactiveBarColor = Color.DarkGray,
+                        activeBarColor = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .size(250.dp)
+                            .align(Alignment.Center),
+                        isTimerRunning = isTimerRunning
+                    )
+                }
+                if(showTimerPlayBtn){
                     StartTimer(
                         modifier = Modifier
                             .align(Alignment.BottomCenter)
-                            .padding(bottom = 74.dp)
+                            .padding(bottom = 74.dp),
+                        isTimerRunning = isTimerRunning,
+                        action = action
                     )
                 }
             }
@@ -104,7 +168,7 @@ private fun TimerContent(
     hour: String,
     minutes: String,
     seconds: String,
-    startTimer: Boolean
+    showTimerPlayBtn: Boolean
 ){
     Column(
         modifier = modifier
@@ -113,7 +177,8 @@ private fun TimerContent(
             hour = hour,
             minutes = minutes,
             seconds = seconds,
-            startTimer = startTimer
+            startTimer = showTimerPlayBtn,
+            action = action
         )
         Spacer(modifier = Modifier.height(40.dp))
         Divider(modifier = Modifier
@@ -164,7 +229,8 @@ private fun TimerRow(
     hour: String,
     minutes: String,
     seconds: String,
-    startTimer: Boolean
+    startTimer: Boolean,
+    action: (TimerActions) -> Unit
 ){
     val color = if(startTimer) MaterialTheme.colors.primary else Color.Unspecified
     Row(
@@ -187,7 +253,9 @@ private fun TimerRow(
             color = color
         )
         IconButton(
-            onClick = { /*TODO*/ },
+            onClick = {
+                action(TimerActions.ClearTimer)
+            },
             modifier = Modifier
                 .padding(top = 23.dp, start = 2.dp)
         ) {
@@ -282,14 +350,104 @@ private fun KeyBoardRow(
 
 
 @Composable
-private fun StartTimer(modifier: Modifier = Modifier){
+private fun StartTimer(
+    modifier: Modifier = Modifier,
+    isTimerRunning: Boolean,
+    action: (TimerActions) -> Unit
+){
     FloatingActionButton(
-        onClick = { /*TODO*/ },
+        onClick = {
+            action(TimerActions.StartTimer)
+        },
         modifier = modifier
     ) {
         Icon(
-            imageVector = Icons.Outlined.PlayArrow,
+            imageVector = if (isTimerRunning) Icons.Outlined.Pause else Icons.Outlined.PlayArrow,
             contentDescription = "start"
         )
     }
 }
+
+
+@Composable
+private fun Timer(
+    totalTime: Long,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    activeBarColor: Color,
+    modifier: Modifier = Modifier,
+    initialValue: Float = 1f,
+    strokeWidth: Dp = 5.dp,
+    isTimerRunning: Boolean,
+){
+    var size by remember{ mutableStateOf(IntSize.Zero)}
+    var value by remember{ mutableStateOf(initialValue)}
+    var currentTime by remember{ mutableStateOf(totalTime)}
+    LaunchedEffect(key1 = currentTime, key2 = isTimerRunning){
+        if(currentTime > 0 && isTimerRunning){
+            currentTime -= 100L
+            value = currentTime / totalTime.toFloat()
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .onSizeChanged {
+                size  = it
+            }
+    ){
+        Canvas(modifier = modifier){
+            drawArc(
+                color = inactiveBarColor,
+                startAngle = 0f,
+                sweepAngle = 360f,
+                useCenter = false,
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+            drawArc(
+                color = activeBarColor,
+                startAngle = 270f,
+                sweepAngle = 360f * value,
+                useCenter = false,
+                size = Size(size.width.toFloat(), size.height.toFloat()),
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+            val center = Offset(size.width / 2f, size.height / 2f)
+            val beta = (360 * value + 270f) * (PI / 180f).toFloat()
+            val r = size.width / 2f
+            val a = kotlin.math.cos(beta) * r
+            val b = sin(beta) * r
+
+            drawPoints(
+                listOf(Offset(center.x + a, center.y + b)),
+                pointMode = PointMode.Points,
+                color = handleColor,
+                strokeWidth = (strokeWidth * 3f).toPx(),
+                cap = StrokeCap.Round
+            )
+        }
+        Text(
+            text = (currentTime / 1000L).toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
